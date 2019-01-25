d6d8d68819f1616bac223ce201081a17d8edb083
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2019-01-25 09:56:44 +0000

    Fixed code style issues.

--- src/functions/authorization/framework/handler.ts
+++ src/functions/authorization/framework/handler.ts
@@ -1,20 +1,20 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult, Context } from 'aws-lambda';
 
-function newCustomAuthorizerResult(principalId: string, effect: string, resource: string): CustomAuthorizerResult {
-  return {
-    principalId: principalId,
+const newCustomAuthorizerResult = (principalId: string, effect: string, resource: string)
+  : CustomAuthorizerResult => ({
+    principalId,
     policyDocument: {
       Version: '2012-10-17', // default version
       Statement: [{
-          Action: 'execute-api:Invoke', // default action
-          Effect: effect,
-          Resource: resource
-      }]
-    }
-  };
-}
+        Action: 'execute-api:Invoke', // default action
+        Effect: effect,
+        Resource: resource,
+      }],
+    },
+  });
 
-export async function handler(event: CustomAuthorizerEvent, fnCtx: Context): Promise<CustomAuthorizerResult> {
+export async function handler(event: CustomAuthorizerEvent, fnCtx: Context)
+  : Promise<CustomAuthorizerResult> {
   const token = event.authorizationToken;
   const userId = 'user-id';
   if (token) {
