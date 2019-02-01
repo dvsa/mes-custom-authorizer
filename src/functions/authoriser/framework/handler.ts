82c1591a322d1c1b005f6d5f01545278d14e1f1b
Rob Shakespeare <robshakespeare@users.noreply.github.com>
2019-02-01 13:57:20 +0000

    Minor code tidy / improvements.

--- src/functions/authoriser/framework/handler.ts
+++ src/functions/authoriser/framework/handler.ts
@@ -6,7 +6,7 @@ import { AdJwtVerifier } from '../application/AdJwtVerifier';
 
 const ensureNotNullOrEmpty = (val: any, fieldName: string) => {
   if (val === null || val === undefined || val === '') {
-    throw `Error: ${fieldName} is null or empty`;
+    throw new Error(`${fieldName} is null or empty`);
   }
 };
 
@@ -20,12 +20,13 @@ const createAdJwtVerifier = async (): Promise<AdJwtVerifier> => {
   ensureNotNullOrEmpty(tenantId, 'tenantId');
   ensureNotNullOrEmpty(applicationId, 'applicationId');
 
-  const openidConfig = await (await nodeFetch(
+  const openidConfigRes = await nodeFetch(
     `https://login.microsoftonline.com/${tenantId}/.well-known/openid-configuration`,
-  )).json();
+  );
+  const openidConfig = await openidConfigRes.json();
 
   if (openidConfig.error_description) {
-    throw `Failed to get openid configuration: ${openidConfig.error_description}`;
+    throw new Error(`Failed to get openid configuration: ${openidConfig.error_description}`);
   }
 
   const jwksclient = jwksClient({
@@ -75,11 +76,10 @@ export async function handler(event: CustomAuthorizerEvent) : Promise<CustomAuth
     return createAuthResult(verifiedToken.unique_name, 'Allow', event.methodArn);
   } catch (err) {
     const failedAuthLogMessage = JSON.stringify({
-      err,
-      event,
       message: 'Failed authorization. Responding with Deny.',
       reason: err && err.toString ? err.toString() : null,
       timestamp: new Date(),
+      err, event, // tslint:disable-line
     });
     console.log(failedAuthLogMessage);
     return createAuthResult('not-authorized', 'Deny', event.methodArn);
