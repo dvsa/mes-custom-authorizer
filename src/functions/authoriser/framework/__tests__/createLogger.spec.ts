dfa0b79619e4b1df558f52cfd6dbb335121d2bba
Ammar Haider <ammar.haider@bjss.com>
2019-02-20 15:41:55 +0000

    adds test for resource existence error logic

--- src/functions/authoriser/framework/__tests__/createLogger.spec.ts
+++ src/functions/authoriser/framework/__tests__/createLogger.spec.ts
@@ -4,7 +4,7 @@ import * as sinon from 'sinon';
 import { uniqueLogStreamName, createLogger } from '../createLogger';
 import { CloudWatchLogs } from 'aws-sdk';
 
-describe('createLogger', () => {
+describe('Logger', () => {
 
   describe('uniqueLogStreamName', () => {
     const sut = uniqueLogStreamName;
@@ -32,29 +32,81 @@ describe('createLogger', () => {
     });
   });
 
-  describe('Logger', () => {
+  describe('createLogger', () => {
 
-    const createLogGroupSpy = sinon.stub().resolves(true);
-    const createLogStreamSpy = sinon.stub().resolves(true);
-    const putLogEventsSpy = sinon.stub().resolves(true);
+    let createLogGroupSpy;
+    let createLogStreamSpy;
+    let putLogEventsSpy;
 
     beforeEach(() => {
 
-      awsSdkMock.mock('CloudWatchLogs', 'createLogGroup', createLogGroupSpy);
-      awsSdkMock.mock('CloudWatchLogs', 'createLogStream', createLogStreamSpy);
-      awsSdkMock.mock('CloudWatchLogs', 'putLogEvents', putLogEventsSpy);
+      awsSdkMock.restore('CloudWatchLogs', 'createLogGroup');
+      awsSdkMock.restore('CloudWatchLogs', 'createLogStream');
+      awsSdkMock.restore('CloudWatchLogs', 'putLogEvents');
 
       process.env.FAILED_LOGINS_CWLG_NAME = 'testLogGroupName';
 
     });
 
     it('should call the correct CloudWatchLogs methods', async () => {
+
+      createLogGroupSpy = sinon.stub().resolves(true);
+      createLogStreamSpy = sinon.stub().resolves(true);
+      putLogEventsSpy = sinon.stub().resolves(true);
+
+      awsSdkMock.mock('CloudWatchLogs', 'createLogGroup', createLogGroupSpy);
+      awsSdkMock.mock('CloudWatchLogs', 'createLogStream', createLogStreamSpy);
+      awsSdkMock.mock('CloudWatchLogs', 'putLogEvents', putLogEventsSpy);
+
       const logger = await createLogger('testLoggerName');
       logger('test error message', 'error');
 
       expect(createLogGroupSpy.calledWith({ logGroupName: 'testLogGroupName' })).toBe(true);
       expect(createLogStreamSpy.called).toBe(true);
       expect(putLogEventsSpy.called).toBe(true);
+    });
+
+    it('should swallow a \"ResourceAlreadyExistsException\" error', async () => {
+
+      createLogStreamSpy = sinon.stub().resolves(true);
+      putLogEventsSpy = sinon.stub().resolves(true);
+
+      awsSdkMock.mock('CloudWatchLogs', 'createLogGroup', async (params) => {
+        throw {
+          errorType: 'ResourceAlreadyExistsException',
+          code: 'ResourceAlreadyExistsException',
+        };
+      });
+      awsSdkMock.mock('CloudWatchLogs', 'createLogStream', createLogStreamSpy);
+      awsSdkMock.mock('CloudWatchLogs', 'putLogEvents', putLogEventsSpy);
+
+      const logger = await createLogger('testLoggerName');
+      logger('test error message', 'error');
+
+      expect(createLogStreamSpy.called).toBe(true);
+      expect(putLogEventsSpy.called).toBe(true);
+    });
+
+    it('should throw on any other exceptions', async (done) => {
+
+      createLogStreamSpy = sinon.stub().resolves(true);
+      putLogEventsSpy = sinon.stub().resolves(true);
+
+      awsSdkMock.mock('CloudWatchLogs', 'createLogGroup', async (params) => {
+        throw {
+          errorType: 'SomeOtherException',
+          code: 'SomeOtherException',
+        };
+      });
+      awsSdkMock.mock('CloudWatchLogs', 'createLogStream', createLogStreamSpy);
+      awsSdkMock.mock('CloudWatchLogs', 'putLogEvents', putLogEventsSpy);
+
+      try {
+        const logger = await createLogger('testLoggerName');
+      } catch (e) {
+        expect(e).toBeTruthy();
+        done();
+      }
     });
   });
 });
