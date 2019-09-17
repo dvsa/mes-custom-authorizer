57a2756dc3d8fded682cb756833b4721ce68dd1e
Ammar <ammar.haider@bjss.com>
2019-09-17 12:18:19 +0100

    adds missing  test coverage for two scenarios

--- src/functions/authoriser/framework/__tests__/handler.spec.ts
+++ src/functions/authoriser/framework/__tests__/handler.spec.ts
@@ -27,13 +27,6 @@ describe('handler', () => {
     moqExtractEmployeeIdFromToken.reset();
 
     mockAdJwtVerifier.setup((x: any) => x.then).returns(() => undefined); // TypeMoq limitation
-    mockVerifyExaminer.setup(x => x(It.isAny()))
-      .returns(() => Promise.resolve({
-        Item: {
-          staffNumber: '12345678',
-          role: 'LDTM',
-        },
-      } as DynamoDB.Types.GetItemOutput));
     mockGetEmployeeIdKey.setup(x => x()).returns(() => 'employeeid');
     moqExtractEmployeeIdFromToken.setup(x => x(It.isAny(), It.isAny())).returns(() => '12345678');
 
@@ -45,8 +38,6 @@ describe('handler', () => {
 
     spyOn(createAdJwtVerifier, 'default')
       .and.returnValue(Promise.resolve(mockAdJwtVerifier.object));
-
-    spyOn(verifyExaminer, 'default').and.callFake(mockVerifyExaminer.object);
     spyOn(getEmployeeIdKey, 'default').and.callFake(mockGetEmployeeIdKey.object);
     spyOn(extractEmployeeIdFromToken, 'extractEmployeeIdFromToken').and.callFake(moqExtractEmployeeIdFromToken.object);
 
@@ -54,6 +45,14 @@ describe('handler', () => {
   });
 
   it('should throw an error if authorizationToken is not set', async () => {
+    mockVerifyExaminer.setup(x => x(It.isAny()))
+      .returns(() => Promise.resolve({
+        Item: {
+          staffNumber: '12345678',
+          role: 'LDTM',
+        },
+      } as DynamoDB.Types.GetItemOutput));
+    spyOn(verifyExaminer, 'default').and.callFake(mockVerifyExaminer.object);
     testCustomAuthorizerEvent.authorizationToken = '';
 
     let errorThrown: Error | undefined;
@@ -84,6 +83,15 @@ describe('handler', () => {
       employeeid: '12345678',
     };
 
+    mockVerifyExaminer.setup(x => x(It.isAny()))
+      .returns(() => Promise.resolve({
+        Item: {
+          staffNumber: '12345678',
+          role: 'LDTM',
+        },
+      } as DynamoDB.Types.GetItemOutput));
+    spyOn(verifyExaminer, 'default').and.callFake(mockVerifyExaminer.object);
+
     mockAdJwtVerifier.setup(x => x.verifyJwt(It.isAny()))
       .returns(() => Promise.resolve(testVerifiedTokenPayload));
 
@@ -105,10 +113,91 @@ describe('handler', () => {
     mockAdJwtVerifier.verify(x => x.verifyJwt('example-token'), Times.once());
   });
 
+  it('should return a default role of "DE" with the correct context when there is no role present', async () => {
+    testCustomAuthorizerEvent.authorizationToken = 'example-token';
+    testCustomAuthorizerEvent.methodArn =
+      'arn:aws:execute-api:region:account-id:api-id/stage-name/HTTP-VERB/resource/path/specifier';
+
+    const testVerifiedTokenPayload: VerifiedTokenPayload = {
+      sub: 'test-subject',
+      unique_name: 'test-unique_name',
+      employeeid: '12345678',
+    };
+
+    mockVerifyExaminer.setup(x => x(It.isAny()))
+      .returns(() => Promise.resolve({
+        Item: {
+          staffNumber: '12345678',
+        },
+      } as DynamoDB.Types.GetItemOutput));
+    spyOn(verifyExaminer, 'default').and.callFake(mockVerifyExaminer.object);
+
+    mockAdJwtVerifier.setup(x => x.verifyJwt(It.isAny()))
+      .returns(() => Promise.resolve(testVerifiedTokenPayload));
+
+    // ACT
+    const result = await sut(testCustomAuthorizerEvent);
+
+    // ASSERT
+    expect(result.policyDocument.Statement[0].Effect).toEqual('Allow');
+    expect((<{ Resource: string }>result.policyDocument.Statement[0]).Resource)
+      .toEqual('arn:aws:execute-api:region:account-id:api-id/stage-name/*/*');
+    expect(result.principalId).toEqual('test-unique_name');
+    expect((<AuthResponseContext>result.context).staffNumber).toBe('12345678');
+    expect((<AuthResponseContext>result.context).examinerRole).toBe('DE');
+
+    mockAdJwtVerifier.verify(x => x.verifyJwt('example-token'), Times.once());
+  });
+
+  it('should return Deny when no employee is found', async () => {
+    testCustomAuthorizerEvent.authorizationToken = 'example-token';
+    testCustomAuthorizerEvent.methodArn =
+      'arn:aws:execute-api:region:account-id:api-id/stage-name/HTTP-VERB/resource/path/specifier';
+    const testVerifiedTokenPayload: VerifiedTokenPayload = {
+      sub: 'test-subject',
+      unique_name: 'test-unique_name',
+      employeeid: '12345678',
+    };
+
+    mockVerifyExaminer.setup(x => x(It.isAny()))
+      .returns(() => Promise.resolve({} as DynamoDB.Types.GetItemOutput));
+    spyOn(verifyExaminer, 'default').and.callFake(mockVerifyExaminer.object);
+
+    mockAdJwtVerifier.setup(x => x.verifyJwt(It.isAny()))
+      .returns(() => Promise.resolve(testVerifiedTokenPayload));
+
+    // ACT
+    const result = await sut(testCustomAuthorizerEvent);
+
+    // ASSERT
+    expect(result.policyDocument.Statement[0].Effect).toEqual('Deny');
+    expect((<{ Resource: string }>result.policyDocument.Statement[0]).Resource)
+      .toEqual('arn:aws:execute-api:region:account-id:api-id/stage-name/*/*');
+    expect(result.principalId).toEqual('not-authorized');
+
+    mockAdJwtVerifier.verify(x => x.verifyJwt('example-token'), Times.once());
+
+    moqFailedAuthLogger.verify(
+      x => x(
+        It.is<string>(s => /Failed authorization\. Responding with Deny\./.test(s)),
+        'error',
+        It.is<any>(o => /The employee id was not found/.test(o.failedAuthReason))),
+      Times.once());
+  });
+
   it('should return Deny when token verification fails', async () => {
     testCustomAuthorizerEvent.authorizationToken = 'example-token';
     testCustomAuthorizerEvent.methodArn =
       'arn:aws:execute-api:region:account-id:api-id/stage-name/HTTP-VERB/resource/path/specifier';
+
+    mockVerifyExaminer.setup(x => x(It.isAny()))
+      .returns(() => Promise.resolve({
+        Item: {
+          staffNumber: '12345678',
+          role: 'LDTM',
+        },
+      } as DynamoDB.Types.GetItemOutput));
+    spyOn(verifyExaminer, 'default').and.callFake(mockVerifyExaminer.object);
 
     mockAdJwtVerifier.setup(x => x.verifyJwt(It.isAny()))
       .throws(new Error('Example invalid token error'));
@@ -144,6 +233,14 @@ describe('handler', () => {
       unique_name: 'test-unique_name',
       employeeid: '12345678',
     };
+    mockVerifyExaminer.setup(x => x(It.isAny()))
+      .returns(() => Promise.resolve({
+        Item: {
+          staffNumber: '12345678',
+          role: 'LDTM',
+        },
+      } as DynamoDB.Types.GetItemOutput));
+    spyOn(verifyExaminer, 'default').and.callFake(mockVerifyExaminer.object);
     mockAdJwtVerifier.setup(x => x.verifyJwt(It.isAny()))
       .returns(() => Promise.resolve(testVerifiedTokenPayload));
 
